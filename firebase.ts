c5ae90a9150e9df21bb07b308a30ca8ac361696c
Benjamin Almendra <benja@Benjamins-MacBook-Air.local>
2025-12-04 19:07:13 -0300

    fix firebase

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,8 @@
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence, browserLocalPersistence } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { Platform } from "react-native";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -18,16 +17,8 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
 const db = getFirestore(app);
-
-let auth: any;
-if (Platform.OS === "web") {
-  auth = initializeAuth(app, {
-    persistence: browserLocalPersistence,
-  });
-} else {
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-  });
-}
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 export { auth, db, app, storage };
